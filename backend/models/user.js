d5e625eb7bfbdc79b872c46af5ef5539cfa8c8ce
Yauheni Kiryienkau <ya@redstream.by>
2018-08-02 13:06:10 +0300

    Users updated

--- backend/models/user.js
+++ backend/models/user.js
@@ -27,10 +27,11 @@ class User {
 
   static async create (role, tel, password, email) {
     tel = String(tel).replace(/[^0-9]/g, "");
-    if (!tel.match(/^375(25|29|33|44|24)[0-9]{7}$/)) error("Кривой номер телефона");
-    if (await db.get('users', tel, { pk: 'tel', fields: 'id'})) error("Телефон уже зарегистрирован");
-    if (await db.get('users', email, { pk: 'email', fields: 'id'})) error("E-mail уже зарегистрирован");
-    const res = await db.query(`INSERT INTO users (role, tel, password, email) VALUES (?,?,PASSWORD(?),?)`, [role, tel, password, email]);
+    if (!tel.match(/^375(25|29|33|44|24)[0-9]{7}$/)) error("Кривой номер телефона", 40001);
+    if (await db.get('users', tel, { pk: 'tel', fields: 'id'})) error("Телефон уже зарегистрирован", 40002);
+    if (await db.get('users', email, { pk: 'email', fields: 'id'})) error("E-mail уже зарегистрирован", 40003);
+    const res = await db.query(`INSERT INTO users (role, tel, password, email) VALUES (?, ?, PASSWORD(?), ?)`,
+        [role, tel, password, email]);
     return res.insertId;
   }
 
