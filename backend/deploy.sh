0a79abca6aa2e7eb25fa049578fdeaa2cf1465dd
Yauheni Kiryienkau <ya@redstream.by>
2018-08-08 10:45:04 +0300

    Deploy upd

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 rsync -av --delete --exclude=uploads ./build/* car@apps.redstream.by:app/ && \
-ssh car@apps.redstream.by "pm2 restart Server"
+ssh car@apps.redstream.by "pm2 restart Server" && \
+echo "Deploy done in ${SECONDS} sec."
