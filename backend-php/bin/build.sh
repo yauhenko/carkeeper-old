2a9e151dc7c359d1b87ba07d11f8933405d38ae4
Yauheni Kiryienkau <ya@redstream.by>
2018-10-12 21:07:16 +0300

    Build script updated

--- backend-php/bin/build.sh
+++ backend-php/bin/build.sh
@@ -3,10 +3,13 @@ bin="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 cd "${bin}/../"
 src=`pwd`
 dst=${src}/build
+tmp=/dev/shm/tmp-bb && \
 echo "Building..."
 cd bin && \
-rm -rf ${dst} && \
-git clone -l -s ../ ${dst} -b master > /dev/null 2>&1 && \
+rm -rf ${dst} ${tmp} && \
+mkdir -p ${dst} && \
+git clone -l -s ../../ ${tmp} -b master > /dev/null 2>&1 && \
+cp -r ${tmp}/backend-php/* ${dst} && \
 cd ${dst} && \
 composer install --no-dev -o > /dev/null 2>&1 && \
 cd bin && \
