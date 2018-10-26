3ee8bb107180513912ab70e26a1ec5f9da5873ed
elfexor <imbalance777@gmail.com>
2018-10-26 17:04:58 +0300

    landing & cp

--- frontend/app/android/app/src/main/java/com/carkeeper/MainActivity.java
+++ frontend/app/android/app/src/main/java/com/carkeeper/MainActivity.java
@@ -1,4 +1,4 @@
-package com.carkeeper;
+package by.carkeeper;
 import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import org.devio.rn.splashscreen.SplashScreen;
--- frontend/app/android/app/src/main/java/com/carkeeper/MainApplication.java
+++ frontend/app/android/app/src/main/java/com/carkeeper/MainApplication.java
@@ -1,4 +1,4 @@
-package com.carkeeper;
+package by.carkeeper;
 
 import android.app.Application;
 
