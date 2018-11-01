494f32da92bfbb85aec892df539c663a1f42f631
Yauheni Kiryienkau <ya@redstream.by>
2018-11-01 21:07:54 +0300

    node.js backend removed (deprecated)

--- backend/deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-rsync -av --delete --exclude=uploads ./build/* car@apps.redstream.by:app/ && \
-ssh car@apps.redstream.by "pm2 restart Server" && \
-echo "Deploy done in ${SECONDS} sec."
