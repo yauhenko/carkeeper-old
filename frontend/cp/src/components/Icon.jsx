7696bcb547cfe1abf912b2c183b9781e15fa918e
Yauheni Kiryienkau <ya@redstream.by>
2018-11-20 19:38:45 +0300

    cp: icons added

---
+++ frontend/cp/src/components/Icon.jsx
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class Icon extends Component {
+
+	static defaultProps = {
+		fixed: false,
+		spin: false,
+		flip: null,
+		zoom: null,
+		rotate: null,
+		extra: ''
+	};
+
+	static propTypes = {
+		icon: PropTypes.string.isRequired,
+		fixed: PropTypes.bool,
+		spin: PropTypes.bool,
+		flip: PropTypes.oneOf(['horizontal', 'vertical']),
+		rotate: PropTypes.oneOf(['90', '180', '270']),
+		zoom: PropTypes.oneOf(['lg', '2x', '3x', '4x', '5x']),
+		extra: PropTypes.string
+	};
+
+	render() {
+		let style = 'fa fa-' + (this.props.icon);
+		if(this.props.fixed) style += ' fa-fw';
+		if(this.props.spin) style += ' fa-spin';
+		if(this.props.zoom) style += ' fa-' + this.props.zoom;
+		if(this.props.flip) style += ' fa-flip-' + this.props.flip;
+		if(this.props.rotate) style += ' fa-rotate-' + this.props.rotate;
+		if(this.props.extra) style += ' ' + this.props.extra;
+		return (<i className={style}/>);
+	}
+}
+
+export default Icon;
--- frontend/cp/src/components/Sidebar/Sidebar.jsx
+++ frontend/cp/src/components/Sidebar/Sidebar.jsx
@@ -1,25 +1,31 @@
-import React, { Component } from 'react';
-import {Link} from "react-router-dom";
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 import {observer, inject} from 'mobx-react';
+import Icon from '../Icon';
 
 @inject('app')
 @observer
 class Sidebar extends Component {
-  render() {
-    return (
-      <div className="sidebar">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/hui">404</Link>
-          </li>
-        </ul>
-		  <button onClick={() => this.props.app.logout(true)}>Logout</button>
-      </div>
-    );
-  }
+
+	btnLogoutClick = () => {
+		this.props.app.logout(true)
+	};
+
+	render() {
+		return (
+			<div className="sidebar">
+				<ul>
+					<li>
+						<Link to="/">Home</Link>
+					</li>
+				</ul>
+				<button onClick={this.btnLogoutClick} className="btn btn-sm btn-danger">
+					<Icon icon="power-off" fixed/>
+					Logout
+				</button>
+			</div>
+		);
+	}
 }
 
 export default Sidebar;
