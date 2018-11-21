c036606523702812db301969cde4906432d82ec0
Yauheni Kiryienkau <ya@redstream.by>
2018-11-21 16:30:30 +0300

    cp: added abstraction level

---
+++ frontend/cp/src/store/AbstractStore.js
@@ -0,0 +1,74 @@
+import {observable, action} from 'mobx';
+import api from '../utils/api';
+
+class AbstractStore {
+
+	endpoint = '';
+
+	@observable meta = null;
+	@observable data = null;
+	@observable refs = null;
+	@observable item = null;
+	@observable page = 1;
+
+	@action fetchList = async () => {
+		try {
+			const res = await api(this.endpoint, { page: this.page, limit: 5 });
+			this.meta = res.meta;
+			this.data = res.data;
+			this.refs = res.refs;
+			return true;
+		} catch (e) {
+			alert(e.message);
+			return false;
+		}
+	};
+
+	@action fetchItem = async (id) => {
+		try {
+			const res = await api(this.endpoint + '/get', {id});
+			this.item = res.user;
+			return true;
+		} catch (e) {
+			alert(e.message);
+			return false;
+		}
+	};
+
+	@action updateItem = async (data = {}) => {
+		this.item = {...this.item, ... data};
+		try {
+			await api(this.endpoint + '/update', { id: this.item.id, user: this.item });
+			await this.fetchList();
+			return true;
+		} catch (e) {
+			alert(e.message);
+			return false;
+		}
+	};
+
+	@action setPage = async (page) => {
+		this.page = page;
+		await this.fetchList()
+	};
+
+	@action delete = async (id, reload = true) => {
+		try {
+			await api(this.endpoint + '/delete', { id });
+			if(reload) await this.fetchList()
+		} catch (e) {
+			alert(e.message);
+		}
+	};
+
+	@action clear = () => {
+		this.meta = null;
+		this.data = null;
+		this.refs = null;
+		this.item = null;
+		this.page = 1;
+	}
+
+}
+
+export default AbstractStore;
--- frontend/cp/src/store/Users.js
+++ frontend/cp/src/store/Users.js
@@ -1,59 +1,8 @@
-import {observable, action} from 'mobx';
-import api from '../utils/api';
+import AbstractStore from './AbstractStore';
 
-class Users {
+class Users extends AbstractStore {
 
-	@observable meta = null;
-	@observable data = null;
-	@observable refs = null;
-	@observable item = null;
-	@observable page = 1;
-
-	@action fetchList = async () => {
-		const res = await api('admin/users', { page: this.page, limit: 5 });
-		this.meta = res.meta;
-		this.data = res.data;
-		this.refs = res.refs;
-	};
-
-	@action fetchItem = async (id) => {
-		const res = await api('admin/users/get', { id });
-		this.item = res.user;
-	};
-
-	@action updateItem = async (data = {}) => {
-		this.item = {...this.item, ... data};
-		try {
-			await api('admin/users/update', { id: this.item.id, user: this.item });
-			await this.fetchList();
-			return true;
-		} catch (e) {
-			alert(e.message);
-			return false;
-		}
-	};
-
-	@action setPage = async (page) => {
-		this.page = page;
-		await this.fetchList()
-	};
-
-	@action delete = async (id, reload = true) => {
-		try {
-			await api('admin/users/delete', { id });
-			if(reload) await this.fetchList()
-		} catch (e) {
-			alert(e.message);
-		}
-	};
-
-	@action clear = () => {
-		this.meta = null;
-		this.data = null;
-		this.refs = null;
-		this.item = null;
-		this.page = 1;
-	}
+	endpoint = 'admin/users';
 
 }
 
