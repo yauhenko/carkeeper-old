30515cd9c8a24354f45e7e53f53a8ee8beff26d6
Yauheni Kiryienkau <ya@redstream.by>
2018-11-21 17:41:47 +0300

    cp: login updated

--- frontend/cp/src/pages/Login/Login.jsx
+++ frontend/cp/src/pages/Login/Login.jsx
@@ -16,7 +16,7 @@ class Login extends Component {
 		e.preventDefault();
 		this.setState({ loading: true });
 		try {
-			await this.props.app.login(e.target.username.value, e.target.password.value);
+			await this.props.app.login(e.target.username.value, e.target.password.value, 3600, e.target.noip.checked);
 			this.props.history.replace('/');
 		} catch (e) {
 			alert(e.message);
@@ -31,16 +31,23 @@ class Login extends Component {
 			<form className="login-form" onSubmit={this.login}>
 				{this.test}
 				<div className="form-group">
-					<label>Tel</label>
+					<label>Телефон</label>
 					<input disabled={this.state.loading} type="text" required={true} autoFocus={!localStorage.username} defaultValue={localStorage.username || ''} name="username" className="form-control"/>
 				</div>
 				<div className="form-group">
-					<label>Password</label>
+					<label>Пароль</label>
 					<input disabled={this.state.loading} type="password" autoFocus={Boolean(localStorage.username)} required={true} name="password" className="form-control"/>
+				</div>
+				<div className="form-group">
+					<label>
+						<input type="checkbox" name="noip"/>
+						&nbsp;
+						Отвязать сессию от IP-адреса
+					</label>
 				</div>
 				<button disabled={this.state.loading} type="submit" className="btn btn-primary">
 					<Icon icon="sign-in" fixed={true}/>
-					Login
+					Войти
 				</button>
 			</form>
 			</div>
