aae1950e12bde06a8fe0f1358985f84089e2228b
Yauheni Kiryienkau <ya@redstream.by>
2018-11-26 13:54:36 +0300

    api: news - removed image

--- backend/src/Controllers/News.php
+++ backend/src/Controllers/News.php
@@ -13,14 +13,11 @@ class News extends ApiController {
 	public function index(): array {
 		$res = Pager::create()
 			->sql('SELECT ** FROM news WHERE published = 1')
-			->fields(['id', 'title', 'content', 'image', 'channel', 'date_begin', 'date_end', 'pinned'])
+			->fields(['id', 'title', 'content', 'channel', 'date_begin', 'date_end', 'pinned'])
 			->order('ORDER BY pinned = 1 DESC, id DESC')
 			->page($this->params->page ?: 1)
 			->limit($this->params->limit ?: 50)
-			->rules([
-				'channel' => ['table' => 'news_channels'],
-				'image' => ['table' => 'uploads'],
-			])->exec()
+			->rules(['channel' => ['table' => 'news_channels']])->exec()
 			->getMetaData(Pager::OPTION_OBJECT_REFS);
 
 		foreach ($res['data'] as &$item) {
@@ -49,7 +46,7 @@ class News extends ApiController {
 	/**
 	 * @route /news/channels
 	 */
-	public function channelsIndex(): array {
+	public function channels(): array {
 		/** @var Client $db */
 		$db = $this->di->db;
 		return [
