b9918103ac47eb673fd516c73c8c87be8f03a3b0
boss <sn1k2006@mail.ru>
2018-12-07 11:34:09 +0300

    login image ios

--- frontend/app/screens/User/Login.js
+++ frontend/app/screens/User/Login.js
@@ -177,9 +177,9 @@ export default class Login extends React.Component {
       <Container>
         <StatusBar backgroundColor="#000" translucent={true} barStyle="light-content"/>
 
-        <Content refreshControl={<RefreshControl enabled={false} refreshing={this.loading}/>} contentContainerStyle={componentStyle.container}>
-          <View style={[StyleSheet.absoluteFill, {alignItems: "center", paddingTop: StatusBar.currentHeight}]}>
-            <Image style={{height: Dimensions.get("window").height - StatusBar.currentHeight, width: Dimensions.get("window").width}} source={background}/>
+        <Content refreshControl={<RefreshControl enabled={false} refreshing={this.loading}/>} contentContainerStyle={componentStyle.container} style={{  backgroundColor: 'rgba(255,255,255,0.4)'}}>
+          <View style={ {alignItems: "center", paddingTop: StatusBar.currentHeight, position: 'absolute', width: Dimensions.get("window").width, top: 0, left: 0, right: 0, bottom: 0 } }>
+            <Image style={{height: '100%', width: '100%'}} source={background}/>
           </View>
 
           <View style={componentStyle.logoContainer}>
