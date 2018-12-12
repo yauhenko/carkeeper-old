ee126d7b6db67af540e09dde26dffb87c3f9c9f9
boss <sn1k2006@mail.ru>
2018-12-12 14:20:56 +0300

    ios toaster

--- frontend/app/components/Notification.js
+++ frontend/app/components/Notification.js
@@ -1,11 +1,10 @@
 import {Platform, ToastAndroid} from "react-native";
-import {Toast} from 'native-base';
+import Toast from 'react-native-simple-toast'
 
 export default function Notification (message = "Произошла ошибка") {
   if (typeof message !== "string") {message = JSON.stringify(message)}
   if(Platform.OS === "android") {
     ToastAndroid.show(message, ToastAndroid.SHORT);
   } else {
-    Toast.show({text: message, duration: 2000})
-  }
+    Toast.show(message, Toast.LONG)}
 }
