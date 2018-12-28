6825f34cc6d5442836d5562d9d083cd767cf8372
elfexor <imbalance777@gmail.com>
2018-12-28 10:04:24 +0300

    member init

---
+++ frontend/member/src/index.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
